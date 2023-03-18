8f15902c457425675c8c2b317cc64a42e4021d12
Sergio Gonzalez <sagr20032006@gmail.com>
2023-03-18 12:32:09 -0500

    Implements Strings as a response on ShoppingCartController

--- tienda/src/main/java/com/shop/controller/ShoppingCartController.java
+++ tienda/src/main/java/com/shop/controller/ShoppingCartController.java
@@ -5,8 +5,6 @@ import com.shop.service.CartService;
 import com.shop.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,28 +15,28 @@ public class ShoppingCartController {
     private ProductService productService;
     @Operation(summary = "Get shopping cart by id")
     @GetMapping(path = "/{idCart}")
-    private ResponseEntity<ShoppingCart> getCart(@PathVariable int idCart) {
-        return ResponseEntity.ok(cartService.getCartById(idCart));
+    private ShoppingCart getCart(@PathVariable int idCart) {
+        return cartService.getCartById(idCart);
     }
     @Operation(summary = "Create shopping cart")
     @PostMapping(path = "")
-    private ResponseEntity<String> createCart(@RequestBody ShoppingCart shoppingCart) {
-        return ResponseEntity.ok(cartService.createCart(shoppingCart));
+    private String createCart(@RequestBody ShoppingCart shoppingCart) {
+        return cartService.createCart(shoppingCart);
     }
     @Operation(summary = "Add item to shopping cart")
     @PutMapping(path = "/{idCart}/{codeProduct}/{quantity}")
-    private ResponseEntity<String> addProduct(@PathVariable int idCart, @PathVariable int codeProduct, @PathVariable int quantity) {
-        return ResponseEntity.ok(cartService.addToCart(idCart, quantity, productService.getProductByCode(codeProduct)));
+    private String addProduct(@PathVariable int idCart, @PathVariable int codeProduct, @PathVariable int quantity) {
+        return cartService.addToCart(idCart, quantity, productService.getProductByCode(codeProduct));
     }
     @Operation(summary = "Remove product by id from shopping cart")
     @PutMapping(path = "/{idCart}/{codeProduct}")
-    private ResponseEntity<String> removeProduct(@PathVariable int idCart, @PathVariable int codeProduct) {
-        return ResponseEntity.ok(cartService.removeFromCart(cartService.getCartById(idCart), codeProduct));
+    private String removeProduct(@PathVariable int idCart, @PathVariable int codeProduct) {
+        return cartService.removeFromCart(cartService.getCartById(idCart), codeProduct);
     }
 
     @Operation(summary = "Sell products from a specific cart")
     @GetMapping(path = "/{idCart}/sell")
-    private ResponseEntity<String> makeSell(@PathVariable int idCart) {
-        return ResponseEntity.ok(cartService.makeSale(cartService.getCartById(idCart)));
+    private String makeSell(@PathVariable int idCart) {
+        return cartService.makeSale(cartService.getCartById(idCart));
     }
 }
