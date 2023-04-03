00799de193bb9b4a4011102b24d8c3320d39c6cc
Sergio Gonzalez <sagr20032006@gmail.com>
2023-04-03 12:05:18 -0500

    Refactor Product class

--- src/main/java/com/shop/model/Product.java
+++ src/main/java/com/shop/model/Product.java
@@ -2,6 +2,8 @@ package com.shop.model;
 
 import javax.persistence.*;
 import java.sql.Date;
+import java.util.Objects;
+
 import lombok.*;
 
 @Entity
@@ -15,10 +17,23 @@ public class Product {
     private int code;
     @Column(name = "NAME")
     private String name;
-    @Column(name = "VALUE")
-    private double unitValue;
+    @Column(name = "UNIT_VALUE")
+    private int unitValue;
     @Column(name = "QUANTITY")
     private int stock;
     @Column(name = "DATE_CREATED")
     private Date dateCreated;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return code == product.code;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code);
+    }
 }
