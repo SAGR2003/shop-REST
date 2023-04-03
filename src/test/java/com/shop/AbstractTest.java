5f53fdb7d0e582def94276017a71502e60e94aa3
Sergio Gonzalez <sagr20032006@gmail.com>
2023-04-03 12:07:49 -0500

    Add local profile with h2

---
+++ src/test/java/com/shop/AbstractTest.java
@@ -0,0 +1,14 @@
+package com.shop;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = "local")
+public abstract class AbstractTest {
+
+}
--- src/test/java/com/shop/ShopApplicationTests.java
+++ src/test/java/com/shop/ShopApplicationTests.java
@@ -2,8 +2,10 @@ package com.shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class ShopApplicationTests {
 
 	@Test
