e6afac5aa2109d00fb07c80ef7dd81694e17dd1f
kareenlama <kareenlatorre03@gmail.com>
2023-05-19 19:42:17 -0500

    Update Swagger documentation

--- src/main/java/com/shop/controller/ProductController.java
+++ src/main/java/com/shop/controller/ProductController.java
@@ -5,23 +5,29 @@ import com.shop.controller.dto.ProductResponseDTO;
 import com.shop.controller.dto.ResponseDTO;
 import com.shop.model.Product;
 import com.shop.service.IProduct;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 @AllArgsConstructor
+@Tag(name = "product-controller", description = "Manage Product items")
 public class ProductController {
     private IProduct productService;
 
     @Operation(summary = "Get all products")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Request Successfull"), @ApiResponse(code = 500, message = "Unexpected error.")})
     @GetMapping(path = "")
     private ListResponseDTO getAllProducts() {
         return new ListResponseDTO(productService.getAllProducts());
     }
 
     @Operation(summary = "Get product by code")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Request Successfull"), @ApiResponse(code = 500, message = "Unexpected error. The product code does not exist"), @ApiResponse(code = 400, message = "Bad request. The product code must be integer and greater than 0")})
     @GetMapping(path = "/{code}")
     private ProductResponseDTO getProduct(@PathVariable int code) {
         Product productByCode = productService.getProductByCode(code);
@@ -35,12 +41,14 @@ public class ProductController {
     }
 
     @Operation(summary = "Create product")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Request Successfull"), @ApiResponse(code = 500, message = "Unexpected error. This product code already exists"), @ApiResponse(code = 400, message = "Bad request. The product code and unit value must be integer and greater than 0")})
     @PostMapping(path = "")
     private ResponseDTO createProduct(@RequestBody Product product) {
         return new ResponseDTO(productService.createProduct(product));
     }
 
     @Operation(summary = "Update stock product by code")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Request Successfull"), @ApiResponse(code = 500, message = "Unexpected error. The product code does not exist"), @ApiResponse(code = 400, message = "Bad request. The quantity to add must be integer and greater than 0")})
     @PutMapping(path = "/{code}/{quantityToAdd}")
     private ResponseDTO updateProduct(@PathVariable int code, @PathVariable int quantityToAdd) {
         return new ResponseDTO(productService.updateProduct(code, quantityToAdd));
--- src/main/java/com/shop/controller/SalesController.java
+++ src/main/java/com/shop/controller/SalesController.java
@@ -6,7 +6,10 @@ import com.shop.controller.dto.ResponseDTO;
 import com.shop.model.AsyncSale;
 import com.shop.model.CartItem;
 import com.shop.service.ISale;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.web.bind.annotation.*;
@@ -16,29 +19,34 @@ import java.util.List;
 @RestController
 @RequestMapping("/sales")
 @AllArgsConstructor
+@Tag(name = "sales-controller", description = "Manage Sales items")
 public class SalesController {
     private ISale saleService;
     private Gson gson;
 
     @Operation(summary = "Get all Sales")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Request Successfull"), @ApiResponse(code = 500, message = "Unexpected error.")})
     @GetMapping(path = "")
     private ListResponseDTO getAllTransactions() {
         return new ListResponseDTO(saleService.getAllSales());
     }
 
     @Operation(summary = "Get sales by document")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Request Successfull"), @ApiResponse(code = 500, message = "Unexpected error. The document client does not exist"), @ApiResponse(code = 400, message = "Bad request. The document client must be integer and greater than 0")})
     @GetMapping(path = "/{documentClient}")
     private ListResponseDTO getTransactionsByDocument(@PathVariable int documentClient) {
         return new ListResponseDTO(saleService.getSalesByDocument(documentClient));
     }
 
     @Operation(summary = "Sell products from a specific sale")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Request Successfull"), @ApiResponse(code = 500, message = "Unexpected error. There is not stock enough and the code does not exists"), @ApiResponse(code = 400, message = "Bad request. Invalid request syntax")})
     @PostMapping(path = "/{documentClient}/sell")
     private ResponseDTO makeSell(@PathVariable int documentClient, @RequestBody List<CartItem> cartItems) {
         return new ResponseDTO(saleService.makeSale(documentClient, cartItems));
     }
 
     @Operation(summary = "Sell products in an asynchronous way")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Request Successfull"), @ApiResponse(code = 500, message = "Unexpected error. There is not stock enough and the code does not exists"), @ApiResponse(code = 400, message = "Bad request. Invalid request syntax")})
     @RabbitListener(queues = "asynchronousSaleQueue")
     private void makeSellAsynchronous(String shoppingCart) {
         AsyncSale asyncSale = gson.fromJson(shoppingCart, AsyncSale.class);
